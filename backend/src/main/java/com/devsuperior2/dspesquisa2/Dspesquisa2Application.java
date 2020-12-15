76d79f4709598e69003be60dd82eb0ea06ae9520
Jose Paulo <paulobrandao356@gmai.com>
2020-12-15 20:26:11 -0300

    Project created

---
+++ backend/src/main/java/com/devsuperior2/dspesquisa2/Dspesquisa2Application.java
@@ -0,0 +1,13 @@
+package com.devsuperior2.dspesquisa2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Dspesquisa2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Dspesquisa2Application.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/devsuperior2/dspesquisa2/Dspesquisa2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior2.dspesquisa2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Dspesquisa2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
